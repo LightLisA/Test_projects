0217ff61287cca26adb63304ab27b7d7e5e7225d
StillTheBest <55080934+LightLisA@users.noreply.github.com>
2024-09-30 22:21:31 +0300

    Delete base_playwright_project-main directory

--- base_playwright_project-main/fixtures/user_auth.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pages.main_page import Main
-
-
-@pytest.fixture(scope='class')
-def user_login(browser):
-    m = Main(browser)
-    m.user_login()
--- base_playwright_project-main/tests/test_auth.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pages.main_page import Main
-
-
-@pytest.mark.smoke
-class TestLogin:
-    def test_user_login(self, browser):
-        m = Main(browser)
-        m.user_login()
